2adafd4020800fe651a499fa5c07a82b8ddf47f6
KachiAlex <opd.livmind@gmail.com>
2026-03-15 06:42:15 +0100

    Keep /admin/jobs/new dynamic

--- src/app/admin/jobs/new/page.tsx
+++ src/app/admin/jobs/new/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 
-export const dynamic = "force-static";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export default function AdminJobsNewRedirect() {
   redirect("/admin/jobs");
